6dc7c2b248a06dd1ed98f3b6ded8ce232b888720
bilenray <raybimlendra@gmail.com>
2022-09-19 02:27:53 -0700

    error solve with some other error

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -2,53 +2,82 @@ import React, { useEffect ,useState} from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 import { useLocation } from 'react-router'
-
+import './Chat.css'
+let i=0;
+let j=0;
+let k=0;
+const ENDPOINT = 'http://localhost:5000';
+console.log("component called")
+let socketID;
+let socket;
 const Chat = () => {
   const location = useLocation()
-  console.log(location)
-  const ENDPOINT = 'http://localhost:5000';
+  console.log("this is search params", location)
 
-  const [Message, addMessage] = useState([]);
-  const [Comment, sendComment] = useState('');
-
-  let socket;
-
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+  
   useEffect(()=>{
+    k= k+1;
+    console.log("k", k)
     const data = queryString.parse(location.search)
     const name= data.name;
     const room= data.room;
+    socket = io(ENDPOINT);
+    console.log('socket id while joining', socket.id)
     console.log('data', data.room)
-    socket = io(ENDPOINT)
-    console.log(socket)
+    console.log("this is socket", socket)
+    console.log("joining the user. Sending login details")
     socket.emit('join', {name, room}, ({error})=>{
-      // console.log('event', error)
+      console.log('Joining error', error)
     })
     return()=>{
+      console.log('disconnection')
       socket.emit('disconnection');
       socket.off();
     }
   }, [ENDPOINT, location])
-
+  
   useEffect(()=>{
+    i= i+1;
+    console.log("i", i)
+    console.log('chat will receive the message from socket.....current messages is', messages, typeof(messages))
+    console.log(message, typeof(message), 'message of setMessage')
     socket.on('message', (message)=>{
-      addMessage([...Message, message]);
-    })
-  }, []);
+      j=j+1;
+      console.log("j", j)
+      socketID= socket.id
+      console.log('after joined', socket.id)
+      console.log("chat received the socket message", message)
+      setMessages([...messages, message])
+      console.log('messages array is here after adding', messages)
+  })
+  }, [])
 
-  const oneMoreComment=(message)=>{
-    console.log(message, 'oneMoreComment message')
-    if (message){
-      socket.emit('sendMessage', message, ()=>{addMessage('')})
+  const sendMessage=(event)=>{
+    
+    event.preventDefault();
+    console.log("checking if there is message (in sendMessage) before sending in socket", message)
+    let socketId = socketID;
+    // console.log('sendMessage and socket-id is', socket.id)
+    if (message !== null){
+      console.log('this is message to be sent in socket', message)
+      socket.emit('sendMessage', {message, socketId}, ()=>setMessage(''))
+      console.log('message sent to the socket')
     }
   }
+
+  console.log("both message and messges", message);
+  console.log(messages)
+
   return (
-    <div>
-      <div className='message'>message</div>
-      <div>
+    <div className='outerContainer'>
+      <div className='container'>
         <input 
-        onChange={(event)=>addMessage(event.target.value)} 
-        onKeyPress={(event)=>event.key==='Enter' ? oneMoreComment(event): null}  
-        type="text" value={Message}/></div>
+        onChange={(event)=>setMessage(event.target.value)} 
+        onKeyPress={(event)=>event.key==='Enter' ? sendMessage(event): null}  
+        type="text" value={message}/>
+      </div>
     </div>
   )
 }
